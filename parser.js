f467c33c31e8d0cad5294ee6a978387822477018
Teo Samaržija <teo.samarzija@gmail.com>
2020-10-30 13:44:53 +0000

    Replace parser.js

--- parser.js
+++ parser.js
@@ -1,5 +1,13 @@
 "use strict";
 function parse(tokenized) {
+    let report="[";
+    for (let i=0; i<tokenized.length; i++)
+       if (i<tokenized.length-1)
+         report+=tokenized[i].getLispExpression()+",";
+       else
+         report+=tokenized[i].getLispExpression();
+    report+=']';
+    console.log("Parsing the expression: "+report);
     let root=new TreeNode("assembly",0); //Value which will be returned.
     for (let i=0; i<tokenized.length; i++) //First, let's deal with the parentheses.
     {
@@ -16,11 +24,11 @@ function parse(tokenized) {
                 j++;
             }
             let newArray=[];
-            for (let k=i; k<j; k++)
+            for (let k=i+1; k<j-1; k++)
                 newArray.push(tokenized[k]);
             tokenized.splice(i+1,j-i-1);
             tokenized[i].text="()";
-            tokenized[i].children=parse(newArray);
+            tokenized[i].children=parse(newArray).children;
         }
     }
     root.children=tokenized;
