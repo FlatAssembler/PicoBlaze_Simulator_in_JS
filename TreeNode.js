ae272bbe44340339a96d993ef019941390e190c2
Teo Samarzija <teo.samarzija@gmail.com>
2020-10-30 21:28:25 +0100

    The parser now seems to work.

--- TreeNode.js
+++ TreeNode.js
@@ -3,17 +3,17 @@ class TreeNode {
   constructor(text, lineNumber) {
     this.text = text;
     this.lineNumber = lineNumber;
-    this.children=[];
+    this.children = [];
   }
   getLispExpression() {
     if (!this.children.length)
-      return '"'+(this.text=='\n'?"\\n":this.text)+'"';
-    let ret="(\""+this.text+"\" ";
-    for (let i=0; i<this.children.length; i++)
-      if (i<this.children.length-1)
-        ret+=this.children[i].getLispExpression()+" ";
+      return '"' + (this.text == "\n" ? "\\n" : this.text) + '"';
+    let ret = '("' + this.text + '" ';
+    for (let i = 0; i < this.children.length; i++)
+      if (i < this.children.length - 1)
+        ret += this.children[i].getLispExpression() + " ";
       else
-        ret+=this.children[i].getLispExpression()+")";
-     return ret;
+        ret += this.children[i].getLispExpression() + ")";
+    return ret;
   }
 }
--- parser.js
+++ parser.js
@@ -1,36 +1,121 @@
 "use strict";
 function parse(tokenized) {
-    let report="[";
-    for (let i=0; i<tokenized.length; i++)
-       if (i<tokenized.length-1)
-         report+=tokenized[i].getLispExpression()+",";
-       else
-         report+=tokenized[i].getLispExpression();
-    report+=']';
-    console.log("Parsing the expression: "+report);
-    let root=new TreeNode("assembly",0); //Value which will be returned.
-    for (let i=0; i<tokenized.length; i++) //First, let's deal with the parentheses.
-    {
-        if (tokenized[i].text=='(') { //As far as I know, PicoBlaze Assembly uses only this type of parentheses.
-            let counter=1;
-            let j=i+1;
-            while (counter) {
-                if (j>=tokenized.length) {
-                    alert("The parenthesis on line "+tokenized[i].lineNumber+" isn't closed!");
-                    return root;
-                }
-                if (tokenized[j].text=='(') counter++;
-                if (tokenized[j].text==')') counter--;
-                j++;
-            }
-            let newArray=[];
-            for (let k=i+1; k<j-1; k++)
-                newArray.push(tokenized[k]);
-            tokenized.splice(i+1,j-i-1);
-            tokenized[i].text="()";
-            tokenized[i].children=parse(newArray).children;
+  let report = "[";
+  for (let i = 0; i < tokenized.length; i++)
+    if (i < tokenized.length - 1)
+      report += tokenized[i].getLispExpression() + ",";
+    else
+      report += tokenized[i].getLispExpression();
+  report += "]";
+  console.log("Parsing the expression: " + report);
+  let root = new TreeNode("assembly", 0); // Value which will be returned.
+  for (
+      let i = 0; i < tokenized.length;
+      i++ // First, let's deal with the parentheses.
+  ) {
+    if (tokenized[i].text == "(") {
+      // As far as I know, PicoBlaze Assembly uses only this type of
+      // parentheses.
+      let counter = 1;
+      let j = i + 1;
+      while (counter) {
+        if (j >= tokenized.length) {
+          alert("The parenthesis on line " + tokenized[i].lineNumber +
+                " isn't closed!");
+          return root;
         }
+        if (tokenized[j].text == "(")
+          counter++;
+        if (tokenized[j].text == ")")
+          counter--;
+        j++;
+      }
+      let newArray = [];
+      for (let k = i + 1; k < j - 1; k++)
+        newArray.push(tokenized[k]);
+      tokenized.splice(i + 1, j - i - 1);
+      tokenized[i].text = "()";
+      tokenized[i].children = parse(newArray).children;
     }
-    root.children=tokenized;
-    return root;
+  }
+  // Dealing with mnemonics and preprocessor directives...
+  for (let i = 0; i < tokenized.length; i++) {
+    if (tokenized[i].text == "\n") {
+      // Delete the new-line characters when you pass over them.
+      tokenized.splice(i, 1);
+      i--;
+      continue;
+    }
+    // Check if the current token is a mnemonic or a preprocessor directive...
+    let isMnemonicOrPreprocessorDirective = false;
+    for (const mnemonic of mnemonics)
+      if (RegExp("^" + mnemonic + "$", "i").test(tokenized[i].text))
+        isMnemonicOrPreprocessorDirective = true;
+    for (const directive of preprocessor)
+      if (RegExp("^" + directive + "$", "i").test(tokenized[i].text))
+        isMnemonicOrPreprocessorDirective = true;
+    if (!isMnemonicOrPreprocessorDirective)
+      continue;
+    // If the current token is a mnemonic or a preprocessor directive, seek for
+    // the next new-line character. Unfortunately, we can't use the C++ find_if
+    // here...
+    let j = i;
+    while (true) {
+      if (j >= tokenized.length) {
+        alert(
+            "Internal compiler error: The assembly-lanaguage expression in line " +
+            tokenized[i].lineNumber + " doesn't end with a new-line token!");
+        return root;
+      }
+      if (tokenized[j].text == "\n")
+        break;
+      j++;
+    }
+    let newArray = [];
+    for (let k = i + 1; k < j; k++)
+      newArray.push(tokenized[k]);
+    tokenized[i].children = parse(newArray).children;
+    tokenized.splice(i + 1, j - i - 1);
+  }
+  // Parsing arithmetic expressions...
+  for (let i = 0; i < tokenized.length; i++)
+    if ((tokenized[i].text == "+" || tokenized[i].text == "-") &&
+        (i == 0 || tokenized[i - 1].text == "," ||
+         tokenized[i - 1].text == "(" || tokenized[i - 1].text == "\n") &&
+        !tokenized[i].children.length) {
+      // Unary operators
+      if (tokenized.length == 1 || tokenized[i + 1].text == "," ||
+          tokenized[i + 1].text == "\n") {
+        alert("Line #" + tokenized[i].lineNumber + ": The unary operator '" +
+              tokenized[i].text + "' has zero operands!");
+        return root;
+      }
+      tokenized[i].children = [
+        TreeNode(0, tokenized[i].lineNumber),
+        tokenized[i + 1],
+      ];
+      tokenized.splice(i + 1, 1);
+    }
+  const parseBinaryOperators = (operators) => {
+    for (let i = 0; i < tokenized.length; i++)
+      if (operators.includes(tokenized[i].text) &&
+          tokenized[i].children.length == 0) {
+        if (i == 0 || tokenized[i - 1].text == "," ||
+            tokenized[i - 1].text == "\n" || i == tokenized.length - 1 ||
+            tokenized[i + 1].text == "," || tokenized[i + 1].text == "\n") {
+          alert("Line #" + tokenized[i].lineNumber + ": The binary operator '" +
+                tokenized[i].text + "' has less than two operands!");
+          return root;
+        }
+        tokenized[i].children = [ tokenized[i - 1], tokenized[i + 1] ];
+        tokenized.splice(i - 1, 1);
+        tokenized.splice(i, 1);
+        i--;
+        continue;
+      }
+  };
+  parseBinaryOperators([ "*", "/" ]);
+  parseBinaryOperators([ "+", "-" ]);
+  root.children = tokenized;
+  return root;
 }
