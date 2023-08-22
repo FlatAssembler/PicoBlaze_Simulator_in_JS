ee77d40c4c72d16ac720d03681c1d0adbb63c01a
Teo Samaržija <teo.samarzija@gmail.com>
2023-08-22 18:38:52 +0200

    Separated the list of directives into a new file (to facilitate testing)

---
+++ list_of_directives.js
@@ -0,0 +1,23 @@
+const mnemonics = [
+  "ADD",     "ADDCY",   "ADDC",    "AND",    "CALL",  "CALL@",       "COMPARE",
+  "COMP",    "DISABLE", "ENABLE",  "FETCH",  "INPUT", "IN",          "JUMP",
+  "JUMP@",   "LOAD",    "OR",      "OUTPUT", "OUT",   "RETURN",      "RET",
+  "RETURNI", "RETI",    "RL",      "RR",     "SL0",   "SL1",         "SLA",
+  "SLX",     "SR0",     "SR1",     "SRA",    "SRX",   "STORE",       "SUB",
+  "SUBCY",   "SUBC",    "TEST",    "XOR",    "INST",  "LOAD&RETURN", "HWBUILD",
+  "STAR",    "OUTPUTK", "REGBANK", "TESTCY", "TESTC", "COMPARECY",   "COMPCY",
+];
+const preprocessor = [
+  "ADDRESS",
+  "ORG",
+  "VHDL",
+  "EQU",
+  "NAMEREG",
+  "CONSTANT",
+  "DISPLAY",
+  "IF",
+  "ELSE",
+  "ENDIF",
+  "WHILE",
+  "ENDWHILE",
+];
