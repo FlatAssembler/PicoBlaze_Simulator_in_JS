6536ebea7f7559677be11cd1893a595c11496896
Teo Samaržija <teo.samarzija@gmail.com>
2023-11-17 13:35:52 +0100

    Refactored `preprocessor.js`

--- preprocessor.js
+++ preprocessor.js
@@ -10,7 +10,8 @@ function isMnemonic(str) {
       return true;
   return false;
 }
-function makeCompilationContext(parsed, oldCompilationContext) {
+function makeCompilationContext(root_of_the_abstract_syntax_tree,
+                                oldCompilationContext) {
   let context;
   if (typeof oldCompilationContext != "undefined")
     context = oldCompilationContext;
@@ -27,7 +28,8 @@ function makeCompilationContext(parsed, oldCompilationContext) {
     context.constants.set("PicoBlaze_Simulator_in_JS", 1);
     context.constants.set("PicoBlaze_Simulator_for_Android", 0);
   }
-  if (!(parsed instanceof TreeNode) || parsed.text != "assembly") {
+  if (!(root_of_the_abstract_syntax_tree instanceof TreeNode) ||
+      root_of_the_abstract_syntax_tree.text != "assembly") {
     // Such an error would be impossible in C++, but there is nothing preventing
     // it in JavaScript.
     alert(
@@ -35,16 +37,18 @@ function makeCompilationContext(parsed, oldCompilationContext) {
     return context;
   }
   let address;
-  for (const node of parsed.children) {
-    if (isMnemonic(node.text)) {
+  for (const node_of_depth_1 of root_of_the_abstract_syntax_tree.children) {
+    if (isMnemonic(node_of_depth_1.text)) {
       if (typeof oldCompilationContext != "undefined") {
         alert(
-            "Line #" + node.lineNumber + ': A mnemonic "' + node.text +
+            "Line #" + node_of_depth_1.lineNumber + ': A mnemonic "' +
+            node_of_depth_1.text +
             '" appears in an if-branching or a while-loop, where only preprocessor directives can appear!');
         return context;
       }
       if (typeof address === "undefined") {
-        alert("Line " + node.lineNumber + ': The mnemonic "' + node.text +
+        alert("Line " + node_of_depth_1.lineNumber + ': The mnemonic "' +
+              node_of_depth_1.text +
               '" appears before any address has been set.');
         return context;
       }
@@ -52,7 +56,7 @@ function makeCompilationContext(parsed, oldCompilationContext) {
                  // works for PicoBlaze (where all directives have the same
                  // size: 18 bits).
     }
-    if (/:$/.test(node.text)) {
+    if (/:$/.test(node_of_depth_1.text)) {
       console.log(
           "DEBUG: Dealing with a label, point #1..."); // Eh, those JavaScript
                                                        // debuggers are worse
@@ -62,105 +66,119 @@ function makeCompilationContext(parsed, oldCompilationContext) {
                                                        // easier than trying to
                                                        // use a debugger.
       if (typeof address === "undefined") {
-        alert("Line " + node.lineNumber + ': The label "' + node.text +
+        alert("Line " + node_of_depth_1.lineNumber + ': The label "' +
+              node_of_depth_1.text +
               '" appears before any address has been set.');
         return context;
       }
-      if (!/^(_|[a-z])\w*:$/i.test(node.text)) {
-        alert("Line " + node.lineNumber + ': "' + node.text +
-              '" is not an allowed label name.');
+      if (!/^(_|[a-z])\w*:$/i.test(node_of_depth_1.text)) {
+        alert("Line " + node_of_depth_1.lineNumber + ': "' +
+              node_of_depth_1.text + '" is not an allowed label name.');
         return context;
       }
       console.log("DEBUG: Dealing with a label, point #2...");
-      context.labels.set(node.text.substr(0, node.text.length - 1), address);
+      context.labels.set(
+          node_of_depth_1.text.substr(0, node_of_depth_1.text.length - 1),
+          address);
       console.log("DEBUG: Dealing with a label, point #3...");
     }
-    if (/^address$/i.test(node.text) || /^org$/i.test(node.text)) {
+    if (/^address$/i.test(node_of_depth_1.text) ||
+        /^org$/i.test(node_of_depth_1.text)) {
       if (typeof oldCompilationContext != "undefined") {
         alert(
-            "Line #" + node.lineNumber +
+            "Line #" + node_of_depth_1.lineNumber +
             ': The pseudo-mnemonic "address" appears in an if-branching or a while-loop, where only preprocessor directives can appear!');
         return context;
       }
       console.log("DEBUG: Setting the address, point #1...");
-      if (node.children.length != 1) {
+      if (node_of_depth_1.children.length != 1) {
         alert(
-            "Line " + node.lineNumber +
+            "Line " + node_of_depth_1.lineNumber +
             ': The "address" pseudo-mnemonic doesn\'t have exactly one argument.');
         return context;
       }
       console.log("DEBUG: Setting the address, point #2...");
-      address =
-          node.children[0].interpretAsArithmeticExpression(context.constants);
+      address = node_of_depth_1.children[0].interpretAsArithmeticExpression(
+          context.constants);
       console.log("DEBUG: Setting the address, point #3...");
     }
-    if (/^constant$/i.test(node.text) || /^equ$/i.test(node.text)) {
+    if (/^constant$/i.test(node_of_depth_1.text) ||
+        /^equ$/i.test(node_of_depth_1.text)) {
       console.log("DEBUG: Setting a constant, point #1...");
-      if (node.children.length != 3) {
+      if (node_of_depth_1.children.length != 3) {
         alert(
-            "Line " + node.lineNumber +
+            "Line " + node_of_depth_1.lineNumber +
             ': The AST node "constant" should have exactly three child nodes (the comma is also an AST node).');
         return context;
       }
-      if (!/^(_|[a-z])\w*$/i.test(node.children[0].text)) {
-        alert("Line " + node.lineNumber + ': "' + node.children[0].text +
+      if (!/^(_|[a-z])\w*$/i.test(node_of_depth_1.children[0].text)) {
+        alert("Line " + node_of_depth_1.lineNumber + ': "' +
+              node_of_depth_1.children[0].text +
               '" is not an allowed constant name.');
         return context;
       }
-      if (node.children[1].text != ",") {
-        alert("Line " + node.lineNumber + ': The second child of the "' +
-              node.text + '" node is "' + node.children[1].text +
+      if (node_of_depth_1.children[1].text != ",") {
+        alert("Line " + node_of_depth_1.lineNumber +
+              ': The second child of the "' + node_of_depth_1.text +
+              '" node is "' + node_of_depth_1.children[1].text +
               '" instead of a comma.');
         return context;
       }
       console.log("DEBUG: Setting a constant, point #2...");
       context.constants.set(
-          node.children[0].text,
-          node.children[2].interpretAsArithmeticExpression(context.constants));
+          node_of_depth_1.children[0].text,
+          node_of_depth_1.children[2].interpretAsArithmeticExpression(
+              context.constants));
       console.log("DEBUG: Setting a constant, point #3...");
     }
-    if (/^namereg$/i.test(node.text)) {
+    if (/^namereg$/i.test(node_of_depth_1.text)) {
       console.log("DEBUG: Naming a register, point #1...");
-      if (node.children.length != 3) {
+      if (node_of_depth_1.children.length != 3) {
         alert(
-            "Line " + node.lineNumber +
+            "Line " + node_of_depth_1.lineNumber +
             ': The AST node "namereg" should have exactly three child nodes (the comma is also an AST node).');
         return context;
       }
-      if (context.namedRegisters.has(node.children[2].text)) {
-        alert("Line " + node.lineNumber + ': Variable named "' +
-              node.children[2].text + '" has already been declared!');
+      if (context.namedRegisters.has(node_of_depth_1.children[2].text)) {
+        alert("Line " + node_of_depth_1.lineNumber + ': Variable named "' +
+              node_of_depth_1.children[2].text +
+              '" has already been declared!');
         return context;
       }
-      if (!/s([0-9]|[a-f])/i.test(node.children[0].text)) {
+      if (!/s([0-9]|[a-f])/i.test(node_of_depth_1.children[0].text)) {
         alert(
-            "Line " + node.lineNumber + ': "' + node.children[0].text +
+            "Line " + node_of_depth_1.lineNumber + ': "' +
+            node_of_depth_1.children[0].text +
             "\" is supposed to be a register name, but it doesn't match the regular expression for registers.");
         return context;
       }
-      if (node.children[1].text != ",") {
-        alert("Line " + node.lineNumber + ': The second child of the "' +
-              node.text + '" node is "' + node.children[1].text +
+      if (node_of_depth_1.children[1].text != ",") {
+        alert("Line " + node_of_depth_1.lineNumber +
+              ': The second child of the "' + node_of_depth_1.text +
+              '" node is "' + node_of_depth_1.children[1].text +
               '" instead of a comma.');
         return context;
       }
       console.log("DEBUG: Naming a register, point #2...");
-      context.namedRegisters.set(node.children[2].text, node.children[0].text);
+      context.namedRegisters.set(node_of_depth_1.children[2].text,
+                                 node_of_depth_1.children[0].text);
       console.log("DEBUG: Naming a register, point #3...");
     }
-    if (/^display$/i.test(node.text) &&
+    if (/^display$/i.test(node_of_depth_1.text) &&
         (
             typeof PicoBlaze !==
             "object" // Because UART_OUTPUT is not declared in PicoBlaze
                      // Simulator for Android, which we detect by `PicoBlaze`
                      // being declared.
             )) {
-      if (node.children[0].text[0] == '"')
+      if (node_of_depth_1.children[0].text[0] == '"')
         document.getElementById("UART_OUTPUT").innerText +=
-            node.children[0].text.substr(1, node.children[0].text.length - 2);
+            node_of_depth_1.children[0].text.substr(
+                1, node_of_depth_1.children[0].text.length - 2);
       else {
         const ASCIIValue =
-            node.children[0].interpretAsArithmeticExpression(context.constants);
+            node_of_depth_1.children[0].interpretAsArithmeticExpression(
+                context.constants);
         if (ASCIIValue != '\n'.charCodeAt(0))
           document.getElementById("UART_OUTPUT")
               .appendChild(
@@ -173,47 +191,52 @@ function makeCompilationContext(parsed, oldCompilationContext) {
                           // GitHub issue about that:
                           // https://github.com/FlatAssembler/PicoBlaze_Simulator_in_JS/issues/8
       }
-    } else if (/^display$/i.test(node.text)) {
-      if (node.children[0].text[0] == '"') {
-        for (let i = 0; i < node.children[0].text.length; i++)
-          if (node.children[0].text[i] != '"')
+    } else if (/^display$/i.test(node_of_depth_1.text)) {
+      if (node_of_depth_1.children[0].text[0] == '"') {
+        for (let i = 0; i < node_of_depth_1.children[0].text.length; i++)
+          if (node_of_depth_1.children[0].text[i] != '"')
             PicoBlaze.displayCharacterOnTerminal(
-                node.children[0].text.charCodeAt(
+                node_of_depth_1.children[0].text.charCodeAt(
                     i)); // Right now, `displayCharacterOnTerminal` is a
                          // no-operation in PicoBlaze_Simulator_for_Android.
       } else {
         PicoBlaze.displayCharacterOnTerminal(
-            node.children[0].interpretAsArithmeticExpression(
+            node_of_depth_1.children[0].interpretAsArithmeticExpression(
                 context.constants));
       }
     }
-    if (/^if$/i.test(node.text) && node.children.length == 2) {
+    if (/^if$/i.test(node_of_depth_1.text) &&
+        node_of_depth_1.children.length == 2) {
       //"if" without "else"
-      if (node.children[0].interpretAsArithmeticExpression(context.constants)) {
-        context = makeCompilationContext(node.children[1], context);
+      if (node_of_depth_1.children[0].interpretAsArithmeticExpression(
+              context.constants)) {
+        context = makeCompilationContext(node_of_depth_1.children[1], context);
       }
-    } else if (/^if$/i.test(node.text) && node.children.length == 3) {
+    } else if (/^if$/i.test(node_of_depth_1.text) &&
+               node_of_depth_1.children.length == 3) {
       // if-else
-      if (node.children[0].interpretAsArithmeticExpression(context.constants))
-        context = makeCompilationContext(node.children[1], context);
+      if (node_of_depth_1.children[0].interpretAsArithmeticExpression(
+              context.constants))
+        context = makeCompilationContext(node_of_depth_1.children[1], context);
       else
-        context = makeCompilationContext(node.children[2], context);
-    } else if (/^if$/i.test(node.text)) {
+        context = makeCompilationContext(node_of_depth_1.children[2], context);
+    } else if (/^if$/i.test(node_of_depth_1.text)) {
       alert(
-          "Line #" + node.lineNumber +
+          "Line #" + node_of_depth_1.lineNumber +
           ': The "if" node should have either 2 or 3 child nodes. This one has ' +
-          node.children.length + " child nodes!");
+          node_of_depth_1.children.length + " child nodes!");
       return context;
     }
-    if (/^while$/i.test(node.text) && node.children.length == 2) {
-      while (
-          node.children[0].interpretAsArithmeticExpression(context.constants)) {
-        context = makeCompilationContext(node.children[1], context);
+    if (/^while$/i.test(node_of_depth_1.text) &&
+        node_of_depth_1.children.length == 2) {
+      while (node_of_depth_1.children[0].interpretAsArithmeticExpression(
+          context.constants)) {
+        context = makeCompilationContext(node_of_depth_1.children[1], context);
       }
-    } else if (/^while$/i.test(node.text)) {
-      alert("Line #" + node.lineNumber +
+    } else if (/^while$/i.test(node_of_depth_1.text)) {
+      alert("Line #" + node_of_depth_1.lineNumber +
             ': The "while" node should have 2 nodes. This one has ' +
-            node.children.length + " child nodes!");
+            node_of_depth_1.children.length + " child nodes!");
     }
   }
   return context;
