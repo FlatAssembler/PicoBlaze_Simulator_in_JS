aae4e5d7d7e6906c08c6eab6eb071cbdf04279d3
Teo Samaržija <teo.samarzija@gmail.com>
2024-04-14 20:04:46 +0200

    Attempting to fix #25

--- sharer.js
+++ sharer.js
@@ -1,34 +1,36 @@
-const btn = document.getElementById("shareButton")
+const btn = document.getElementById("shareButton");
 
 btn.addEventListener("click", (e) => {
-    console.log("====\nSaving the program====\n")
-    saveAssemblyCode()
-})
+  console.log("====\nSaving the program====\n");
+  saveAssemblyCode();
+});
 
-    function saveAssemblyCode() {
-      const assemblyCode = document.getElementById("assemblyCode").innerText;
+function saveAssemblyCode() {
+  const assemblyCode = document.getElementById("assemblyCode").innerText;
 
-      const formData = new FormData();
-      formData.append('code', assemblyCode);
+  const formData = new FormData();
+  formData.append("code", assemblyCode);
 
-      fetch('db.php', {method : 'POST', body : formData})
+  fetch("db.php", {method : "POST", body : formData})
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
 
-          .then(response => {
-            if (!response.ok) {
-              throw new Error('Network response was not ok');
-            }
+        return response.text();
+      })
 
-            return response.text();
-          })
+      .then((data) => {
+        // data is ?id=int
+        const message = "Share URL: ";
+        const shareURL =
+            `${new URL(window.location.href).origin}/PicoBlaze.html${data}`;
+        if (/WebPositive/.test(navigator.userAgent))
+          // WebPositive seems not to support the "prompt" directive.
+          alert(message + shareURL);
+        else
+          prompt(message, shareURL);
+      })
 
-          .then(data => {
-            // data is ?id=int
-
-            const message = "Share URL: ";
-            prompt(message,
-                   `${new URL(window.location.href).origin}/PicoBlaze.html${
-                       data}`);
-          })
-
-          .catch(error => { console.error(error); });
-    }
+      .catch((error) => { console.error(error); });
+}
--- viewer.js
+++ viewer.js
@@ -1,27 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
   const urlParams = new URLSearchParams(window.location.search);
 
-  if (urlParams.has('id')) {
-    const id = urlParams.get('id');
+  if (urlParams.has("id")) {
+    document.getElementById("assemblyCode").innerHTML =
+        "Fetching the program from SourceForge...";
+    const id = urlParams.get("id");
     const url = `db.php?id=${encodeURIComponent(id)}`;
     console.log(url);
     fetch(url)
-        .then(response => {
+        .then((response) => {
           if (!response.ok) {
-            throw new Error('Network response was not ok');
+            throw new Error("Network response was not ok");
           }
 
           return response.text();
         })
 
-        .then(data => {
+        .then((data) => {
           const asm = document.getElementById("assemblyCode");
           data = data.replace("\r\n", "\n");
-          document.getElementById('assemblyCode').innerText = data;
+          document.getElementById("assemblyCode").innerText = data;
           console.log(asm.textContent);
           setUpLineNumbers();
         })
 
-        .catch(error => { console.error('Error:', error); });
+        .catch((error) => { console.error("Error:", error); });
   }
 });
