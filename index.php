bbcc6887d96676ec25cf3fb17b8c486df27e47a1
Teo Samaržija <teo.samarzija@gmail.com>
2024-05-22 12:07:11 +0200

    Removed the possibility of `index.php` being used for XSS

--- index.php
+++ index.php
@@ -1,36 +1,4 @@
 <?php
-
-require_once 'db.php';
-
-// view a program
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-
-    if ($id == "") {
-        echo "NO";
-        return;
-    }
-
-    $stmt = $conn->prepare("SELECT code FROM programs WHERE id = :id");
-    $stmt->bindParam(':id', $id);
-    $stmt->execute();
-
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($result) {
-        $programCode = $result['code'];
-
-        $htmlContent = file_get_contents("PicoBlaze.html");
-
-        $renderedHtml = str_replace("{{PROGRAM_CODE}}", $programCode, $htmlContent);
-
-        echo $renderedHtml;
-    } else {
-        echo "Program not found!";
-    }
-} else {
     header("Location: PicoBlaze.html");
-    exit;
-}
 ?>
 Click <a href="PicoBlaze.html">here</a> in case your browser does not automatically redirect you.
