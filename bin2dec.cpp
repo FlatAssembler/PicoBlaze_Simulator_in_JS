f900a8abad67e989a6d49b8b19decb3fd000a16a
Teo Samaržija <teo.samarzija@gmail.com>
2025-09-24 11:23:06 +0200

    Warned that the program does not work in GCC when optimizer is enabled

--- bin2dec.cpp
+++ bin2dec.cpp
@@ -1,31 +1,39 @@
 /*
- *  Compile on 64-bit Linux, like this:
- *  g++ -o bin2dec bin2dec.cpp -std=c++11
- *  Playing with inline assembly is a lot easier on Linux than on Windows.
+ *  Compile on 64-bit Linux or Solaris (I guess it will probably also work on
+ * FreeBSD), like this: g++ -o bin2dec bin2dec.cpp -std=c++11 #Don't put -O3
+ * here, as GCC seems to ignore `volatile` in global variables. Or like this:
+ *  clang++ -o bin2dec bin2dec.cpp -O3
+ *  Playing with inline assembly is a lot easier on UNIX-like systems than on
+ * Windows.
  * */
 
 #include <iomanip>
 #include <iostream>
 
 extern "C" {
-char binary_input[9];
-int first_digit, binary_coded_decimal;
+char binary_input[9]; // I think this does not need to be volatile because the
+                      // inline assembly isn't changing it. It's only reading
+                      // from it.
+volatile int first_digit,
+    binary_coded_decimal; // GCC (but not CLANG) clobbers the `first_digit` if
+                          // you turn on the optimizer in spite of the
+                          // `volatile`.
 }
 
 int main() {
   std::cout << "Enter a binary number containing at most 8 digits: ";
   std::cin >> binary_input;
   asm(R"assembly(
-	.intel_syntax noprefix
+        .intel_syntax noprefix
 	
-	mov r8, 0
+        mov r8, 0
         mov r9, 0
         input_loop:
           cmp byte ptr [binary_input + r9], '0'
           jz input_is_zero
           cmp byte ptr [binary_input + r9], '1'
           jz input_is_one
-          jmp end_of_the_input_loop
+          jmp end_of_the_input_loop # The current character is presumably '\0'.
           input_is_one:
             shl r8, 1
             inc r8
@@ -59,11 +67,11 @@ int main() {
           jmp loop_for_dividing_r8_by_10
         end_of_the_loop_for_dividing_r8_by_10:
         
-        shl r9, 4 #Multiply r9 by 16.
+        shl r9, 4 # Multiply r9 by 16.
         add r9, r8
         mov qword ptr [binary_coded_decimal], r9
 
-	.att_syntax
+        .att_syntax
 	)assembly");
   std::cout << "The number converted to decimal is: " << std::hex
             << std::setfill('0') << std::setw(2) << first_digit
