8650d41360db541e209840885244180985876355
Teo Samaržija <teo.samarzija@gmail.com>
2025-11-07 13:05:50 +0100

    Refactored the PHP a bit

---
+++ db_helper.php
@@ -0,0 +1,43 @@
+<?php
+
+$GLOBALS['dbname'] = "p3379031_assembler_db";
+
+class Database {
+
+    private static $instance;
+    private $connection;
+
+    private function __construct() {
+        $servername = "mysql-p";
+        $username = "p3379031rw";
+        $password = substr(file_get_contents(".env"), strlen("password="));
+        if (substr($password, -1, 1) == "\n") {
+            $password = substr($password, 0, strlen($password) - 1);
+        }
+        if (substr($password, -1, 1) == "\r") {
+            $password = substr($password, 0, strlen($password) - 1);
+        }
+        $dbname = $GLOBALS['dbname'];
+
+        try {
+            $this->connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            http_response_code(500);
+            die("Connection to the database failed: " . $e->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
+
+?>
