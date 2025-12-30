f69d78fceb1a8f63db4750a2976ca93cf5dbff1b
Teo Samaržija <46264381+FlatAssembler@users.noreply.github.com>
2025-12-30 19:38:55 +0100

    Update headerScript.js

--- headerScript.js
+++ headerScript.js
@@ -403,7 +403,7 @@ function drawTable() {
       is_UART_enabled ? "" : "checked"} onchange="deletePCpointer()" /></td>
 <td style="text-align:left; border-left: none;"><label for="shouldWeUpdateRegisters">Update registers and flags on every step</label></td></tr>
 <tr><td colspan="2" style="font-family: Arial; text-align: justify; font-weight: normal; border-top: none;">
-Displaying registers and flags on every step is useful for debugging, but it slows the simulation down (due to <a href="https://web.dev/articles/avoid-large-complex-layouts-and-layout-thrashing#avoid_layout_thrashing">layout thrashing</a>). I would not recommend you to enable UART and updating the registers and flags at the same time.
+Displaying registers and flags on every step is useful for debugging, but it slows the simulation down (due to <a href="https://web.dev/articles/avoid-large-complex-layouts-and-layout-thrashing#avoid_layout_thrashing">layout thrashing</a>). I would not recommend you to enable UART and updating the registers and flags at the same time. And in case you think you are safe from that because you use a modern Internet browser, reality check: <a href="https://www.reddit.com/r/firefox/comments/18icm8l/why_does_firefox_52_seem_to_run_javascript_faster/?utm_source=share&utm_medium=web2x&context=3">Modern Internet browsers deal with layout thrashing worse than Firefox 52 (the last version of Firefox runnable on Windows XP) deals with it</a>.
 </td></tr></table>
 <table style="font-size:0.95em;">
 <tr>
