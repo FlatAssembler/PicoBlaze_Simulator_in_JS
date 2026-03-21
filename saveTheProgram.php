b004af51c3d2a57d8a172a00ea66c24d0ed0615d
Teo Samarzija <teo.samarzija@gmail.com>
2026-03-21 17:16:47 +0100

    Attempted to get the saving to work (not tested)

---
+++ saveTheProgram.php
@@ -0,0 +1,18 @@
+<?php
+	if (!isset($_SESSION['username']))
+		die("The username is not set in the session!");
+	if (!isset($_POST['code']))
+		die("The client has not sent the code to be saved!");
+
+	include 'db_helper.php';
+	$conn = Database::getInstance()->getConnection();
+
+	$stmt = $conn->prepare("SELECT id FROM usernames WHERE username = ?");
+	$stmt->bind_param('s', $_SESSION['username']);
+	$stmt->execute();
+	$id=$stmt->get_result()->fetch_assoc()['id'];
+
+	$stmt = $conn->prepare("UPDATE codes_belonging_to_users SET code=? WHERE id=?");
+	$stmt->bind_param('si',$_POST['code'],$id);
+	die("SUCCESS");
+?>
